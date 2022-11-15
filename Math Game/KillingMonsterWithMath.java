79429305a50284f08d3fec00d24df1b03563e0c2
xyval <111503969+xyval@users.noreply.github.com>
2022-11-15 18:16:48 +0800

    Update KillingMonsterWithMath.java

--- Math Game/KillingMonsterWithMath.java
+++ Math Game/KillingMonsterWithMath.java
@@ -1,4 +1,72 @@
+//imports: 
+
 public class KillingMonsterWithMath
 {
+    public static void main(String[]arg) 
+    {
 
+    }
+
+    //print out rules & any welcome message or smt 
+    public static void rules ()
+    {
+
+    }
+
+    //ask for level 
+    public static int level ()
+    {
+        int level = 0; 
+        return level;
+    }
+
+    //generate question, print question, return correct answer 
+    public static int Q1 () 
+    {
+        int Q1 = 0;
+        return Q1;
+    }
+    public static int Q2 ()
+    {
+        int Q2 = 0;
+        return Q2;
+    }
+    public static int Q3 ()
+    {
+        int Q3 = 0;
+        return Q3;
+    }
+
+    //show question 
+    public static void showQ ()
+    {
+
+    }
+
+    //get user's answer, show guide if they need help->(could ignore if you don't want :P)
+    public static int userAns ()
+    {
+        int userAns = 0;
+        return userAns;
+    }
+
+    public static void showMonster ()
+    {
+        
+    }
 }
+
+/* For Planning:
+    explain rules
+    repeat: 
+        ask for what level
+        generate question
+        show question
+        show guide if need help 
+        get user's answer
+        get correct answer
+        check answer
+        record score 
+        add score # of circles 
+        show monster 
+ */
